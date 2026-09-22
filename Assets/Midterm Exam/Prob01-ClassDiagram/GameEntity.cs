6b70cee7de5701c8be7d882d1729a769da2ac2ad
SXUNDAYY <phitchayapa.chuc@bumail.net>
2026-09-22 10:44:54 +0700

    finish1

--- Assets/Midterm Exam/Prob01-ClassDiagram/GameEntity.cs
+++ Assets/Midterm Exam/Prob01-ClassDiagram/GameEntity.cs
@@ -4,6 +4,20 @@ namespace MidtermExam.Prob01
 {
     public class GameEntity
     {
-        // TODO: Implement fields and methods according to Class Diagram
+        public string id;
+        private Vector3 position;
+        protected int health;
+        public virtual void Update()
+        {
+            
+        }
+        public virtual void TakeDamage(int damage)
+        {
+            
+        }
+        private void Move(Vector3 direction)
+        {
+            
+        }
     }
 }
--- Assets/Midterm Exam/Prob01-ClassDiagram/InventoryItem.cs
+++ Assets/Midterm Exam/Prob01-ClassDiagram/InventoryItem.cs
@@ -2,8 +2,14 @@ using UnityEngine;
 
 namespace MidtermExam.Prob01
 {
-    public class InventoryItem
+    public class InventoryItem 
     {
-        // TODO: Implement fields and methods according to Class Diagram
+        public string itemName;
+        protected int weight;
+        private int itemValue;
+        public virtual void Use(Character user)
+        {
+            
+        }
     }
 }
